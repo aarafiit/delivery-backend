21b511881b08065ccaf2455159d956b5224b2594
abdullah.rafi <rafiabdullah748@gmail.com>
2026-02-04 21:51:50 +0600

    initialized project

---
+++ src/test/java/kn/org/deliverybackend/DeliveryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kn.org.deliverybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
